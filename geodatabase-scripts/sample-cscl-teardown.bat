a1ae055fd7a27ad3b5219feba78b854efcb557d8
mattyschell <mattyschell@gmail.com>
2025-07-21 11:54:55 -0400

    adds sample script update to match previous commit

--- geodatabase-scripts/sample-cscl-teardown.bat
+++ geodatabase-scripts/sample-cscl-teardown.bat
@@ -1,10 +1,18 @@
 set ENV=xxx
 set TARGETGDB=C:\xxx\yyy.sde
+set TARGETSCHEMA=CSCL
+set TARGETPASSWORD=xxx
+set TARGETDB=xxxxxxx
 set BASEPATH=C:\xxx
 set TARGETLOGDIR=%BASEPATH%\cscl-migrate\geodatabase-scripts\logs\
-set PROPY=C:\Progra~1\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe
+set PROPY=C:\Users\%USERNAME%\AppData\Local\Programs\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe
 set BATLOG=%TARGETLOGDIR%%ENV%-cscl-teardown.log
-echo starting cscl teardown of %TARGETGDB% on %date% at %time% > %BATLOG%
-%PROPY% %BASEPATH%\cscl-migrate\src\py\teardown-cscl-migrate.py %TARGETGDB%
+echo starting %ENV% cscl teardown of %TARGETGDB% on %date% at %time% > %BATLOG%
+echo. >> %BATLOG% && echo starting reveal_all_history in %TARGETSCHEMA% on %TARGETDB% on %date% at %time% >> %BATLOG%
+sqlplus %TARGETSCHEMA%/"%TARGETPASSWORD%"@%TARGETDB% @src/sql/reveal_all_history.sql
+sqlplus %TARGETSCHEMA%/"%TARGETPASSWORD%"@%TARGETDB% @src/sql/deregister_all_archiving.sql
+%PROPY% %BASEPATH%\cscl-migrate\src\py\teardown-cscl-migrate.py %TARGETGDB% listofarchivelists
+%PROPY% %BASEPATH%\cscl-migrate\src\py\teardown-cscl-migrate.py %TARGETGDB% listoflists
 echo. >> %BATLOG% && echo review the log at %TARGETLOGDIR% >> %BATLOG%
-echo. >> %BATLOG% && echo ending cscl teardown of %TARGETGDB% >> %BATLOG%
+echo. >> %BATLOG% && echo completed %ENV% cscl teardown of %TARGETGDB% on %date% at %time%  >> %BATLOG%
+   
