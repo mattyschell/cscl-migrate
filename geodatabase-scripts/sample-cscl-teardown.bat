14616805f1d7b8bf8dcdc4965561898403931d74
mattyschell <mattyschell@gmail.com>
2025-07-30 09:28:58 -0400

    revises teardown procedure closes #28

--- geodatabase-scripts/sample-cscl-teardown.bat
+++ geodatabase-scripts/sample-cscl-teardown.bat
@@ -9,10 +9,7 @@ set PROPY=C:\Users\%USERNAME%\AppData\Local\Programs\ArcGIS\Pro\bin\Python\envs\
 set BATLOG=%TARGETLOGDIR%%ENV%-cscl-teardown.log
 echo starting %ENV% cscl teardown of %TARGETGDB% on %date% at %time% > %BATLOG%
 echo. >> %BATLOG% && echo starting reveal_all_history in %TARGETSCHEMA% on %TARGETDB% on %date% at %time% >> %BATLOG%
-sqlplus %TARGETSCHEMA%/"%TARGETPASSWORD%"@%TARGETDB% @src/sql/reveal_all_history.sql
-sqlplus %TARGETSCHEMA%/"%TARGETPASSWORD%"@%TARGETDB% @src/sql/deregister_all_archiving.sql
-%PROPY% %BASEPATH%\cscl-migrate\src\py\teardown-cscl-migrate.py %TARGETGDB% listofarchivelists
 %PROPY% %BASEPATH%\cscl-migrate\src\py\teardown-cscl-migrate.py %TARGETGDB% listoflists
-echo. >> %BATLOG% && echo review the log at %TARGETLOGDIR% >> %BATLOG%
+echo. >> %BATLOG% && echo review the logs at %TARGETLOGDIR% >> %BATLOG%
 echo. >> %BATLOG% && echo completed %ENV% cscl teardown of %TARGETGDB% on %date% at %time%  >> %BATLOG%
    
